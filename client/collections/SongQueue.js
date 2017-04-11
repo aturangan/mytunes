465658570d0f31cf7eb7629a4533c8a262f7d58f
Hack Reactor Student <student@hackreactor.com>
2017-04-11 12:32:57 -0700

    tests

--- client/collections/SongQueue.js
+++ client/collections/SongQueue.js
@@ -3,8 +3,38 @@ var SongQueue = Backbone.Collection.extend({
 
   model: SongModel,
 
-  initialize: function() {
+  playFirst: function() {
+  	// this.models[0].play();
+  	this.at(0).play(); 
+  },
 
+  initialize: function() {
+  	//enqueue first
+  	//if length === 1 then playFirst 
+
+
+  	this.on('add', function() { 
+  		console.log('MODELS', JSON.stringify(this.models))
+
+  		if (!this.at(1)) {
+  			this.playFirst(); 
+  		}
+    })
+
+    this.on('ended', function() {
+    	this.shift(); 
+
+    	if (this.length > 0) {
+    		this.playFirst();
+    	}
+    });
+
+    this.on('dequeue', function() {
+    	this.remove(); 
+    })
   }
+
+
+
 
 });
--- client/views/SongQueueEntryView.js
+++ client/views/SongQueueEntryView.js
@@ -1,4 +1,20 @@
 // SongQueueEntryView.js - Defines a backbone view class for the song queue entries.
 var SongQueueEntryView = Backbone.View.extend({
   // your code here!
+  // initialize: function() {
+  // 	this.render(); 
+  // },
+
+  template: _.template('<td>(<%= artist %>)</td><td><%= title %></td>'),
+
+  render: function(){
+  	//return this.$el.html(this.template(this.model.attributes));
+
+
+    this.$el.html('<th>Queue</th>').append(
+      this.collection.map(function(song) {
+        return new SongQueueEntryView({model: song}).render();
+      })
+    );
+  }
 });
